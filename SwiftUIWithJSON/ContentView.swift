e15edc9eb58a6b0f640fff7c7b23554a1878f68a
Sachindra Fernando <sachindrafernando3@gmail.com>
2025-07-22 13:35:02 +0100

    Initial Commit

---
+++ SwiftUIWithJSON/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIWithJSON
+//
+//  Created by Sachindra Fernando on 2025-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIWithJSON/SwiftUIWithJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithJSONApp.swift
+//  SwiftUIWithJSON
+//
+//  Created by Sachindra Fernando on 2025-07-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
